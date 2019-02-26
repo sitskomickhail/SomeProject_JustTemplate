b416f1a18eac79eba396c902ef4d11d70044c69f
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2019-02-26 19:55:06 +0300

    Add files via upload

---
+++ SomeProject.PL.Desktop/ViewModels/TeamViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using SomeProject.BLL.DTO;
+using SomeProject.BLL.Interfaces;
+using Unity.ServiceLocator;
+
+namespace SomeProject.PL.Desktop.ViewModels
+{
+	public class TeamViewModel
+	{
+		private ObservableCollection<TeamInfo> _teams;
+		private readonly ITeamService _teamService;
+		public TeamViewModel()
+		{
+			_teamService = ServiceLocator.Current.GetInstance<ITeamService>();
+		}
+
+		public ObservableCollection<TeamInfo> Teams
+		{
+			get
+			{
+				if (_teams == null)
+				{
+					var result = _teamService.GetAll();
+					if (result.IsSuccess)
+					{
+						_teams = new ObservableCollection<TeamInfo>(result.Value);
+					}
+					else
+					{
+						throw  new Exception(result.Message);
+					}
+				}
+
+				return _teams;
+			}
+		}
+
+
+	}
+}
