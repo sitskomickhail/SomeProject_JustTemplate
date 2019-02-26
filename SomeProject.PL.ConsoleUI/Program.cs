24a1c15dba4a82e3f7b18366b9e94f027f2e9710
Mikhail <31074312+sitskomickhail@users.noreply.github.com>
2019-02-26 19:53:51 +0300

    Add files via upload

---
+++ SomeProject.PL.ConsoleUI/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using SomeProject.BLL.Interfaces;
+using SomeProject.BLL.Services;
+using Unity;
+
+
+namespace SomeProject.PL.ConsoleUI
+{
+	class Program
+	{
+		
+		static void Main()
+		{
+
+			var container = new UnityContainer();
+			container.RegisterType<ITeamService, TeamService>();
+			container.RegisterType<IPlayerService, PlayerService>();
+
+			var viewModel = new ViewModel(container);
+
+			var team = viewModel.CreateTeam("FC Barcelona");
+			var player = viewModel.CreatePlayer("Messi", "Leonel", new DateTime(1982, 10, 10), team.Id);
+
+
+			var teams = viewModel.GetTeams();
+			foreach (var item in teams)
+			{
+				Console.WriteLine($"{item.Name} - count: {item.CountPlayers}");
+			}
+
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ SomeProject.PL.ConsoleUI/ViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using SomeProject.BLL.DTO;
+using SomeProject.BLL.Helpers;
+using SomeProject.BLL.Interfaces;
+using Unity;
+
+namespace SomeProject.PL.ConsoleUI
+{
+	public class ViewModel
+	{
+		private readonly ITeamService _teamService;
+		private readonly IPlayerService _playerService;
+
+		public ViewModel(IUnityContainer container)
+		{
+			_teamService = container.Resolve<ITeamService>();
+			_playerService= container.Resolve<IPlayerService>();
+		}
+
+
+		public IEnumerable<TeamInfo> GetTeams()
+		{
+			var result = _teamService.GetAll();
+			if (result.IsSuccess)
+			{
+				return result.Value;
+			}
+
+			throw new Exception(result.Message);
+		}
+
+		public TeamInfo CreateTeam(string name)
+		{
+			var result = _teamService.Create(name);
+			if(result.IsSuccess)
+			{
+				return result.Value;
+			}
+
+			throw new Exception(result.Message);
+		}
+
+		public PlayerFullInfo CreatePlayer(string lastName, string firstName, DateTime birthDay, int teamId)
+		{
+			var itemInfo = new PlayerFullInfo()
+			{
+				LastName = lastName,
+				FirstName = firstName,
+				BirthDay = birthDay,
+				TeamId = teamId
+			};
+			var result = _playerService.Create(itemInfo);
+			if (result.IsSuccess)
+			{
+				return result.Value;
+			}
+
+			throw new Exception(result.Message);
+		}
+
+
+
+		public void AddPlayerToTeam(int teamId, int playerId)
+		{
+
+		}
+
+	}
+}
